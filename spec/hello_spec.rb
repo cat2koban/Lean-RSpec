e489e2a1f4fe86b5f83791b08e366e9daa9184c3
cat2koban <minamiedamame0328@gmail.com>
2019-08-20 22:11:20 +0900

    modify spec をまとめた

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -6,89 +6,17 @@ require_relative '../lib/hello'
 #   end
 # end
  
-RSpec.describe '四則演算' do
-  it ' 1 + 1 は 2 になること' do
-    expect(1 + 1).to eq 2
-  end
-
-  it '10 - 1 は 9 になること' do
-    expect(10 - 1).to eq 9
-  end
-end
-
 RSpec.describe User do
   describe '#greet' do
-    it '12歳以下の場合、ひらがなで答えること' do
-      user = User.new(name: 'たろう', age: 12)
-      expect(user.greet).to eq 'ぼくはたろうだよ。'
-    end
-    it '13歳以上の場合、漢字で答えること' do
-      user = User.new(name: 'たろう', age: 13)
-      expect(user.greet).to eq '僕はたろうです。'
-    end
-  end
-end
-
-RSpec.describe User do
-  describe '#greet' do
+    let(:user) { User.new(name: 'たろう', age: age) }
+    subject { user.greet }
     context '12歳以下の場合' do
-      it 'ひらがなで答えること' do
-        user = User.new(name: 'たろう', age: 12)
-        expect(user.greet).to eq 'ぼくはたろうだよ。'
-      end
+      let(:age) { 12 }
+      it { is_expected.to eq 'ぼくはたろうだよ。' }
     end
     context '13歳以上の場合' do
-      it '漢字で答えること' do
-        user = User.new(name: 'たろう', age: 13)
-        expect(user.greet).to eq '僕はたろうです。'
-      end
+      let(:age) { 13 }
+      it { is_expected.to eq '僕はたろうです。' }
     end
   end
 end
-
-RSpec.describe User do
-  describe '#greet' do
-    before do
-      @params = { name: 'たろう' }
-    end
-    context '12歳以下の場合' do
-      it 'ひらがなで答えること' do
-        user = User.new(@params.merge(age: 12))
-        expect(user.greet).to eq 'ぼくはたろうだよ。'
-      end
-    end
-    context '13歳以上の場合' do
-      it '漢字で答えること' do
-        user = User.new(@params.merge(age: 13))
-        expect(user.greet).to eq '僕はたろうです。'
-      end
-    end
-  end
-end
-
-RSpec.describe User do
-  describe '#greet' do
-    before do
-      @params = { name: 'たろう' }
-    end
-    context '12歳以下の場合' do
-      before do
-        @params.merge!(age: 12)
-      end
-      it 'ひらがなで答えること' do
-        user = User.new(@params)
-        expect(user.greet).to eq 'ぼくはたろうだよ。'
-      end
-    end
-    context '13歳以上の場合' do
-      before do
-        @params.merge!(age: 13)
-      end
-      it '漢字で答えること' do
-        user = User.new(@params)
-        expect(user.greet).to eq '僕はたろうです。'
-      end
-    end
-  end
-end
-
