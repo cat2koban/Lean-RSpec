e5be9cf9ce8be8cc5c5c92ed30b8812d01d69e60
cat2koban <minamiedamame0328@gmail.com>
2019-08-20 22:22:58 +0900

    modify shared_examples を利用した example の再利用化

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -1,22 +1,33 @@
 require_relative '../lib/hello'
 
-# RSpec.describe Hello do
-#   it "message return hello" do
-#     expect(Hello.new.message).to eq "hello"
-#   end
-# end
- 
 RSpec.describe User do
   describe '#greet' do
     let(:user) { User.new(name: 'たろう', age: age) }
     subject { user.greet }
-    context '12歳以下の場合' do
-      let(:age) { 12 }
+
+    shared_examples '子どものあいさつ' do
       it { is_expected.to eq 'ぼくはたろうだよ。' }
     end
-    context '13歳以上の場合' do
-      let(:age) { 13 }
+    context '0歳の場合' do
+      let(:age) { 0 }
+      it_behaves_like '子どものあいさつ'
+    end
+    context '12歳の場合' do
+      let(:age) { 12 }
+      it_behaves_like '子どものあいさつ'
+    end
+
+    shared_examples '大人のあいさつ' do
       it { is_expected.to eq '僕はたろうです。' }
+    end
+    context '13歳の場合' do
+      let(:age) { 13 }
+      it_behaves_like '大人のあいさつ'
+    end
+    context '100歳の場合' do
+      let(:age) { 100 }
+      it_behaves_like '大人のあいさつ'
     end
   end
 end
+
