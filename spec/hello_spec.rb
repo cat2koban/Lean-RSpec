d6409d8c28fef5a1c0d03f6d8d6b37dfa1812b16
cat2koban <minamiedamame0328@gmail.com>
2019-08-20 22:33:12 +0900

    add child?メソッドのテスト

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -29,5 +29,19 @@ RSpec.describe User do
       it_behaves_like '大人のあいさつ'
     end
   end
-end
 
+  describe '#child?' do
+    let(:user) { User.new(name: 'たろう', age: age) }
+    subject { user.child? }
+
+    context '12歳以下の場合' do
+      let(:age) { 12 }
+      it { is_expected.to eq true }
+    end
+
+    context '13歳以下の場合' do
+      let(:age) { 13 }
+      it { is_expected.to eq false }
+    end
+  end
+end
