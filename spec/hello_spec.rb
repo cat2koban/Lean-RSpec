005475c2edfc4d65358de5dc711fc6ec2d32305d
cat2koban <minamiedamame0328@gmail.com>
2019-08-21 11:15:01 +0900

    add pending のサンプルテスト

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -36,4 +36,14 @@ RSpec.describe User do
       it { is_expected.to eq false }
     end
   end
+
+  describe '繊細なクラス' do
+    it '繊細なテスト' do
+      expect(1 + 2).to eq 3
+
+      pending 'この先はなぜかテストが失敗するのであとで直す'
+      # パスしないエクスペクテーション(実行される)
+      expect(foo).to eq bar
+    end
+  end
 end
